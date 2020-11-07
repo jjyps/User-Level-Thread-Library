8b9cda48b5fd33f41f4b784aafed606bdbc0c5ac
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2020-11-06 21:21:38 -0800

    Added Queue and Queue Tester Implementation to Report

--- REPORT.md
+++ REPORT.md
@@ -1,9 +1,24 @@
 # User Level Thread Library Report
 
 ### Make sure each line has a max of 80 chars
+### Queue Implementation
+In *queue.c*, we used a doubly linked list to implement the queue, where each node has a pointer to the next and previous node. We felt like a doubly linked list would be more sufficient and flexible than an array.
+- *queue_create* allocates memory for the queue and initializes the head and tail of the list. *queue_destroys* on the other hand, deallocates the queue's memory.
+- *queue_enqueue* creates a new node and adds it at the end of the list by linking it with the tail node through *prev* and *next* variables.
+- *queue_delete* iterates through the list until it finds a match. When it does it links the previous node and next node and then deallocates the memory created by the corresponding node.
+- *queue_iterate* iterates through the array and execute the given function with each element in the queue.
+### Queue Tester
+*queue_tester* file contains 19 testcases in order to detect any error in the *queue* implementation. We created testcases for each aspect of every function in the *queue* API.
+- *queue_destroy* function is tested when it’s supposed to succeed, when queue is not empty, and when queue is NULL.
+- *queue_enqueue* and *queue_dequeue* are tested via several functions in the tester. It tests both functions with both large and small number of elements. It does this by checking if *queue_dequeue* removes the right element and if all elements were added or removed using *queue_length*. In addition, it also handles edge cases when the queue is NULL or empty.
+- *queue_delete* is tested with both large and small elements. As well as, edge cases such as a NULL queue or data. In addition, it also tests that the first element is deleted if there are duplicates.
+- *queue_iterate* is tested with two functions *even_to_zero* and *odd_to_zero*. 
+    - *odd_to_zero* changes elements with odd values to zero and deletes elements with even values. The tester, therefore, tests if even elements were successfully deleted and all odd elements were successfully changed to zero.
+    - *even_to_zero* only changes elements with even values to zero, without deleting any element.
 
-### Challenges/Limitations faced
-
+### Challenges/Limitations Faced
+One of the challenges we faced is with implementing user-level thread library API. It was difficult to try to imagine how threads would be run since it is a relatively new concept to us. In addition, integrating the functions given to us in our API implementation without being able to see some of the functions' implementation. Another challenged we faced is having to figure out a bug that was occurring in a function used by *uthread_ctx_switch*.
+  
 ### References
 
 ###### By *Gharam Alsaedi* & *Seunghyup Alex Oh*
--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -12,7 +12,7 @@
 #include "context.c"
 enum State { running = 0,ready = 1,blocked = 2,zombie = 3,terminated = 4, idle = 5 };
 static queue_t run_threads;
-static struct uthread_tcb *curr_thread;
+static struct uthread_tcb curr_thread;
 //static struct uthread_tcb* idle_thread;
 struct uthread_tcb
 {
@@ -27,25 +27,25 @@ struct uthread_tcb
 struct uthread_tcb *uthread_current(void)
 {
 	/* TODO Phase 2 */
-	return curr_thread;
+	return &curr_thread;
 }
 
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
-	struct uthread_tcb *prev_thread = uthread_current();
+	struct uthread_tcb prev_thread = curr_thread;
 	void *temp_thread;
 	queue_dequeue(run_threads, (void **)&temp_thread);
 	queue_enqueue(run_threads, &prev_thread);
-	struct uthread_tcb *next_thread = (struct uthread_tcb *)get_head(run_threads);
+	struct uthread_tcb next_thread = *(struct uthread_tcb *)get_head(run_threads);
 	curr_thread = next_thread;
-	uthread_ctx_switch(&prev_thread->thread_context, &next_thread->thread_context);
+	uthread_ctx_switch(&prev_thread.thread_context, &next_thread.thread_context);
 }
 
 void uthread_exit(void)
 {
 	/* TODO Phase 2 */
-	curr_thread->curr_state = 3;
+	curr_thread.curr_state = 3;
 }
 
 int uthread_create(uthread_func_t func, void *arg) //Done, I think?
@@ -79,32 +79,32 @@ int uthread_start(uthread_func_t func, void *arg)
 	/* TODO Phase 2 */
 	/* initialize first thread */
 	uthread_create(func, arg);
-	curr_thread = (struct uthread_tcb *)get_head(run_threads);
-	curr_thread->curr_state = 5;
+	curr_thread = *(struct uthread_tcb *)get_head(run_threads);
+	curr_thread.curr_state = 5;
 
 	/*Loop until there are no more threads */
 	while (queue_length(run_threads) != 0)
 	{
 		/* if thread is not ready to run then yield */
-		if ((curr_thread->curr_state != 1 && curr_thread->curr_state != 5) || (curr_thread->curr_state == 5 && queue_length(run_threads) > 1))
+		if ((curr_thread.curr_state != 1 && curr_thread.curr_state != 5) || (curr_thread.curr_state == 5 && queue_length(run_threads) > 1))
 		{
 			uthread_yield();
 		}
 
 		/* execute function */
 		func(arg);
-		curr_thread->curr_state = 0;
+		curr_thread.curr_state = 0;
 		uthread_exit();
 		/*Thread was completed then deallocate stack, delete from queue & yield to next one */
-		if (curr_thread->curr_state == 3)
+		if (curr_thread.curr_state == 3)
 		{
-			uthread_ctx_destroy_stack(curr_thread->top_of_stack);
-			curr_thread->curr_state = 4;
+			uthread_ctx_destroy_stack(curr_thread.top_of_stack);
+			curr_thread.curr_state = 4;
 			struct uthread_tcb *prev_thread;
 			queue_dequeue(run_threads, (void **)&prev_thread);
 			if (queue_length(run_threads) == 0)
 				break;
-			uthread_yield();
+			// uthread_yield();
 		}
 	}
 	queue_destroy(run_threads);
@@ -115,7 +115,7 @@ int uthread_start(uthread_func_t func, void *arg)
 void uthread_block(void)
 {
 	/* TODO Phase 2/3 */
-	curr_thread->curr_state = 2;
+	curr_thread.curr_state = 2;
 }
 
 void uthread_unblock(struct uthread_tcb *uthread)
