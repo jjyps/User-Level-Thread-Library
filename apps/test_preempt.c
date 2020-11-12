72d09541eb6bc4da9b3885ece157e9d62973f18b
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2020-11-11 16:35:00 -0800

    t

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -2,9 +2,23 @@
 #include <stdio.h>
 #include <preempt.c>
 #include <unistd.h>
+#include <uthread.h>
 
+void thread2(void *arg) {
+    printf("Hello im thread2...");
+    printf("im going to be annoying and run forever\n");
+    while(1) {
+        printf("forever\n");
+    }
+}
+void thread1(void *arg) {
+    printf("Hello I am thread 1\n");
+    uthread_create(thread2, NULL);
+    uthread_yield();
+    printf("Im back, never should've trusted thread 2\n");
+    exit(0);
+}
 int main(void) {
-
-
+    uthread_start(thread1, NULL);
     return 0;
 }
