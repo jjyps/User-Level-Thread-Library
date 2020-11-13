cad9777535b8390b9c228c558cf5ce855102e42f
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2020-11-12 18:11:53 -0800

    fixed coding style

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -12,7 +12,7 @@ void thread2(void *arg) {
     }
 }
 void thread1(void *arg) {
-    printf("Hello I am thread 1\n");
+    printf("Hello I am thread 1, im going to be nice and yield\n");
     uthread_create(thread2, NULL);
     uthread_yield();
     printf("Im back, never should've trusted thread 2\n");
